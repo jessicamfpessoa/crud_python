a04ae72c542bd8465d7d5d40c687ee35ee841ad6
pessoajess <pessoamfjess@gmail.com>
2024-12-08 19:25:08 -0300

    creating method read all and ready by id

--- crud_livros.py
+++ crud_livros.py
@@ -2,7 +2,7 @@ import psycopg2
 
 class Livros:
     def __init__(self, id_livro, nome_livro, autor, num_paginas, ano_lancamento, editora):
-        self.id_livro = None
+        self.id_livro = id_livro
         self.nome = nome_livro
         self.autor = autor
         self.paginas = num_paginas
@@ -40,25 +40,51 @@ class CrudLivros:
         self.conn.commit()
 
     def insert(self, livro):
-        query = '''
+        query = f'''
             INSERT INTO public.livro(
-            id_livro, 
-            nome_livro, 
-            autor, 
-            num_paginas, 
-            ano_lancamento, 
-            editora
-        );
+                id_livro, 
+                nome_livro, 
+                autor, 
+                num_paginas, 
+                ano_lancamento, 
+                editora
+            )
+            VALUES(
+                {livro.id_livro},
+                '{livro.nome}',
+               '{livro.autor}',
+                {livro.paginas},
+                {livro.lancamento},
+                '{livro.editora}'
+            );
         '''
+        cur = self.conn.cursor()
+        cur.execute(query)
+        self.conn.commit()
 
-    def read_all(self, livro):
+    def read_all(self):
         query = '''
             SELECT *
             FROM public.livro
         '''
 
-    def read_by_id(self, livro):
-        pass
+        cur = self.conn.cursor()
+        cur.execute(query)
+
+        return cur.fetchall()
+
+    def read_by_id(self, id_livro):
+        query = f'''
+                   SELECT *
+                   FROM public.livro
+                   WHERE id_livro = {id_livro}
+               '''
+
+        cur = self.conn.cursor()
+        cur.execute(query)
+
+        return cur.fetchall()
+
 
     def update(self, livro):
         pass
@@ -76,10 +102,22 @@ crud = CrudLivros(
 
 # crud.create_table()
 sda = Livros(
-    id_livro=1,
-    nome_livro='Senhor dos Anéis - As Duas Torres',
-    autor= 'Tolkien',
+    id_livro = 1,
+    nome_livro = "Senhor dos Anéis - As Duas Torres",
+    autor = "Tolkien",
     num_paginas = 1000,
-    ano_lancamento= 1945,
-    editora = 'Rocco'
+    ano_lancamento = 1945,
+    editora = "Rocco"
 )
+
+hp = Livros(
+    id_livro = 2,
+    nome_livro = "Harry POtter - A Pedra Filosofal ",
+    autor = "J.K. Rolling",
+    num_paginas = 350,
+    ano_lancamento = 1997,
+    editora = "Intríseca"
+)
+# crud.insert(hp)
+
+crud.read_by_id(2)
